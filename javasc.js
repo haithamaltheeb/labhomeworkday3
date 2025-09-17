705021681a9bee39764cc855f3d99b993d8a4c25
Haitham AlTheeb <haitham.deeb8@gmail.com>
2025-09-17 04:35:12 +0300

    this_is_day3

---
+++ javasc.js
@@ -0,0 +1,20 @@
+//console.log(about) this is error
+document.getElementById("title").textContent = "Welcome to Haitham's Blog";
+
+console.log("Changed <h1> text");
+
+document.getElementById("myname").textContent = "Haitham D.";
+
+console.log("Changed #myname text");
+
+document.title = "Haitham's Blog Website";
+
+console.log("Changed document title");
+
+document.getElementById("body").style.backgroundColor = "#e6f7ff";
+
+console.log("Changed background color");
+
+document.getElementById("title").style.color = "darkred";
+
+console.log("Changed <h1> color");
